1050c617692c269ae0e34f50c2d2ac2295c6b1f5
Cristiano Cunha <contato@cristianoprogramador.com>
2021-09-06 00:41:41 -0300

    Cria Docker-Compose com HeathCehck

--- src/consumer/log/rabbitConnection.js
+++ src/consumer/log/rabbitConnection.js
@@ -1,10 +1,9 @@
 const amqp = require('amqplib');
+const CONNECTION_STRING = process.env.CONNECTION_STRING;
 
 const getConnection = async () => {
-    const CONNECTION_STRING = 'amqp://cristiano:123456789@localhost:5672/';
     const connectionFactory = await amqp.connect(CONNECTION_STRING);
     const connection = await connectionFactory.createChannel();
-
     return connection;
 }
 
